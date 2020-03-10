66671b53e671fba91208af7eb1b31e0dd2e9d639
ZHJ0125 <shandonghoujin@163.com>
2020-03-11 00:08:19 +0800

    Update Ex5_8

---
+++ Exercise/ex5_8/ex5_8.c
@@ -0,0 +1,64 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void h_exit(int status){
+	if(WIFEXITED(status)){
+		printf("Normal termination, exit status = %d\n", WEXITSTATUS(status));
+	}
+	else if(WIFSIGNALED(status)){
+		printf("Abnormal termination, signal number = %d %s\n", WTERMSIG(status),
+	#ifdef WCOREDUMP
+		WCOREDUMP(status)?")":"(core file generated)");
+	#else
+		")");
+	#endif
+	}
+}
+
+int main(){
+	pid_t pid;
+	int status;
+	#ifdef WCOREDUMP
+		printf("Defined WCOREDUMP\n");
+	#endif
+	if((pid = fork()) < 0){
+		printf("Fork error!\n");
+		exit(0);
+	}
+	else if(pid == 0){
+		exit(7);
+	}
+	if(wait(&status) != pid){
+		printf("Wait error!\n");
+		exit(0);
+	}
+	h_exit(status);
+	if((pid = fork()) < 0){
+		printf("fork error!\n");
+		exit(0);
+	}
+	else if(pid == 0){
+		abort();
+	}
+	if(wait(&status) != pid){
+		printf("Wait() failed\n");
+		exit(0);
+	}
+	h_exit(status);
+	if((pid = fork()) < 0){
+		printf("fork error!\n");
+		exit(0);
+	}
+	else if(pid == 0){
+		status /= 0;
+	}
+	if(wait(&status) != pid){
+		printf("Wait() failed\n");
+		exit(0);
+	}
+	h_exit(status);
+	exit(0);
+}
