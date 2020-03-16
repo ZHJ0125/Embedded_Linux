15f016b381db3a85cdd349cf192ae78585b48d00
ZHJ0125 <shandonghoujin@163.com>
2020-03-16 14:46:24 +0800

    Update Ex5_15

--- Exercise/ex5_15/ex5_15.c
+++ Exercise/ex5_15/ex5_15.c
@@ -24,26 +24,43 @@ int main(int argc, char* argv[]){
         printf("Usage: %s cmd1 cmd2 ... \n", argv[0]);
         return -1;
     }
-    setbuf(stdout, NULL);
+    setbuf(stdout, NULL);   // 将标准输出流中的数据清空
+    // 循环进入每一条命令
     for(i = 1, p = argv[1]; argc > 1; p = argv[1]){
+        // Ｐ是指向单条命令的指针，输出命令序号和命令内容
         printf("===== [%d]: command '%s' begin =====\n", i, p);
         if((pid = fork()) == 0){
+            // 获取子进程的PID
             printf("Child pid = %d\n", getpid());
+            // 逐条执行命令
             execl(argv[1], argv[1], NULL);
-            exit(argc);
+            exit(argc);     // 执行失败返回命令序号
         }
+        // 父进程等待子进程结束
         pid = wait(&stat);
+        // 判断并输出子进程的结束信息
         statport(pid, stat);
         argc--; argv++;
+        // 输出命令序号和命令内容
         printf("===== [%d]: command '%s' end =====\n\n", i++, p);
     }
 }
 
+/**
+* Description: 用来判断和输出子进程的结束状态
+* Param: 
+*   - int pid: 子进程的标识号
+*   - int stat: 子进程的终止状态信息
+* Return: void
+* Date: 2020/03/16
+*/
 void statport(int pid, int stat){
     int i;
     if(pid == -1){
+        // wait函数错误，没有子进程
         printf("Bad wait\n");
     }
+    //　下面是根据stat状态信息，判断子进程的返回状态
     else if((stat&0177) == 0177){
         i = stat>>8;
         printf("Child process: %d stop by signal\n", pid);
