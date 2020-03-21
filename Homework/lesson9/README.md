67bb3a7083234c38bf12c88f07b5231064a8fbe8
ZHJ0125 <shandonghoujin@163.com>
2020-03-21 10:28:11 +0800

    Upload Lesson9 Homework

---
+++ Homework/lesson9/README.md
@@ -0,0 +1,13 @@
+# 程序说明
+
+## 实现的功能
+
+1. 循环显示 "How are you!"
+2. 设置循环显示标志位
+3. Ctrl+C组合键后修改循环显示标志位，中断循环显示，显示 "Bye bye!"
+4. 使用信号注册函数
+5. 使用延时函数缓冲循环过程
+
+## 测试过程
+
+![测试过程](https://github.com/ZHJ0125/Embedded_Linux/blob/master/Image/Homework/lesson9/lesson9_show.gif)
---
+++ Homework/lesson9/lesson9.c
@@ -0,0 +1,26 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void sigcatcher(int signum);
+int flag = 1;
+
+int main(){
+    // Registering signal handlers
+    if((signal(SIGINT, &sigcatcher)) == SIG_ERR){
+        printf("Couldn't register signal handler\n");
+        exit(1);
+    }
+    // Cyclic display based on flag
+    while(flag){
+        printf("How are you!\n");
+        usleep(100000);
+    }
+    return 0;
+}
+
+void sigcatcher(int signum){
+    flag = 0;
+    printf("Bye bye!\n");
+}
