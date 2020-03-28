0fc92d3a1b3d6006c045173844fe049bbd881dce
ZHJ0125 <shandonghoujin@163.com>
2020-03-28 18:40:19 +0800

    Update ex6_5 comment

--- Exercise/ex6_5/ex6_5.c
+++ Exercise/ex6_5/ex6_5.c
@@ -7,25 +7,33 @@ static void sig_quit(int);
 
 int main(){
     sigset_t newmask, oldmask, pendmask;
+    // 绑定信号处理函数
     if(signal(SIGQUIT, &sig_quit) == SIG_ERR){
         printf("Couldn't register signal handler for SIGQUIT\n");
         exit(1);
     }
+    // 清空信号掩码
     sigemptyset(&newmask);
+    // 添加 SIGQUIT 信号至信号掩码中
     sigaddset(&newmask, SIGQUIT);
 
+    // 增加信号
     if(sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0){
         printf("SIG_BLOCK error\n");
         exit(2);
     }
+    // 延时5秒
     sleep(5);
+    // pendmask 返回被阻塞的信号集合
     if(sigpending(&pendmask) < 0){
         printf("sigpending error!\n");
         exit(3);
     }
+    // 检测 pendmask 里面是否含有 SIGQUIT 信号
     if(sigismember(&pendmask, SIGQUIT)){
         printf("SIGQUIT pending\n");
     }
+    // 重置信号掩码
     if(sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0){
         printf("SIG_SETMASK error!\n");
         exit(4);
