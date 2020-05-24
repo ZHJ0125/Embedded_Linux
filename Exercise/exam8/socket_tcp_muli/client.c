aa98f2cee5938ff0552e00bdbb86535335f8735b
ZHJ0125 <shandonghoujin@163.com>
2020-05-24 18:06:46 +0800

    Update Socket TCP

--- Exercise/exam8/socket_tcp_muli/client.c
+++ Exercise/exam8/socket_tcp_muli/client.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <signal.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <unistd.h>
-// #define SERVER_IP "127.0.0.1"		// 用于本地测试
-#define SERVER_IP "47.95.13.239"	// 用于公网测试
-#define SERVER_PORT 18888
+/********************************************************************
+*   File Name: client.c
+*   Description: 用于实现多客户端通信
+*   Others: 
+*     1. 客户端只需考虑如何连接目标服务器即可
+*     2. 客户端首先创建套接字并向服务器附送网络连接请求
+*     3. 连接到服务器之后，创建子进程
+*     4. 父进程用来向服务器发送数据，子进程从服务器接收数据
+*   Init Date: 2020/05/24
+*********************************************************************/
+#include "mytcp.h"
 
 int main(int argc, char *argv[]){
 	int client_sockfd;
@@ -17,37 +17,42 @@ int main(int argc, char *argv[]){
     char buf_recv[BUFSIZ];                                 	//数据传送的缓冲区
 	char buf_send[BUFSIZ];
 	struct sockaddr_in remote_addr;                         //服务器端网络地址结构体
-	memset(&remote_addr, 0, sizeof(remote_addr));           //数据初始化--清零
 
+	/* 初始化目标服务器的网络信息 */
+	memset(&remote_addr, 0, sizeof(remote_addr));           //数据初始化清零
 	remote_addr.sin_family = AF_INET;                       //设置为IP通信
 	remote_addr.sin_addr.s_addr = inet_addr(SERVER_IP);     //服务器IP地址
 	remote_addr.sin_port = htons(SERVER_PORT);              //服务器端口号
-	
+
 	/*创建客户端套接字--IPv4协议，面向连接通信，TCP协议*/
 	if((client_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		perror("socket error");
 		return 1;
 	}
-	
+
 	/*将套接字绑定到服务器的网络地址上*/
 	if(connect(client_sockfd, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) < 0){
 		perror("connect error");
 		return 1;
 	}
 	printf("connected to server\n");
+
+	/* 从服务器接收初始化的握手消息 */
 	len = recv(client_sockfd, buf_recv, BUFSIZ, 0);			//接收服务器端信息
     buf_recv[len] = '\0';
-	printf("%s", buf_recv); //打印服务器端的欢迎信息
+	printf("%s", buf_recv);									//打印服务器端的欢迎信息
 	printf("Enter string to send: \n");
-	
+
+	/* 创建父子进程与服务器进行通信 */
 	if((pid = fork()) < 0){
 		printf("Fail to call fork()\n");
 		return 1;
 	}
+	// 父进程用来发送数据
 	else if(pid > 0){
-		// 父进程用来发送数据
 		while(1){
 			scanf("%s", buf_send);
+			getchar();
 			if(!strcmp(buf_send, "quit")){
 				kill(pid, SIGSTOP);
 				break;
@@ -55,14 +60,15 @@ int main(int argc, char *argv[]){
 			len = send(client_sockfd, buf_send, strlen(buf_send), 0);
 		}
 	}
+	// 子进程用来接收数据
 	else{
-		// 子进程用来接收数据
 		while(1){
 			memset(buf_recv, 0, sizeof(buf_recv));
 			len = recv(client_sockfd, buf_recv, BUFSIZ, 0);
 			buf_recv[len] = '\0';
 			printf("Recive from server: %s\n", buf_recv);
 		}
+		usleep(300000);
 	}
 
 	/*关闭套接字*/
---
+++ Exercise/exam8/socket_tcp_muli/mytcp.h
@@ -0,0 +1,44 @@
+/********************************************************************
+*   File Name: mytcp.h
+*   Description: 用于实现多客户端通信
+*   Init Date: 2020/05/24
+*********************************************************************/
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <signal.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define SERVER_IP "127.0.0.1"       // 用于本地测试
+// #define SERVER_IP "47.95.13.239"	// 用于公网测试
+#define SERVER_PORT 18888
+#define HANDSHARK_MSG "Hello,Client!\n"
+#define MaxClientNum 10
+
+/* 套接字信息结构体，用于记录客户端信息 */
+typedef struct MySocketInfo{
+    int socketCon;                  // 套接字描述符
+    char *ipaddr;                   // 客户端IP地址
+    uint16_t port;                  // 客户端端口号
+}_MySocketInfo;
+
+char buffer[BUFSIZ];                // 服务器数据收发缓冲区
+int ReadyToSend;                    // 服务器准备发送标志位
+
+/* 用于记录客户端信息的数组 */
+struct MySocketInfo arrConSocket[MaxClientNum];
+int conClientCount;                 // 当前客户端数量
+
+/* 用来与客户端通信的线程数组 */
+pthread_t arrThrReceiveClient[MaxClientNum];
+int thrReceiveClientCount;          // 当前通信子线程数量
+
+/* 线程功能函数 */
+void *fun_thrReceiveHandler(void *socketInfo);
+void *fun_thrAcceptHandler(void *socketListen);
+int checkThrIsKill(pthread_t thr);
